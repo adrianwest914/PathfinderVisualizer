a745523bf6321e8d7427656b7a14713edd2d9910
Opuli1 <72239450+Opuli1@users.noreply.github.com>
2021-04-09 20:00:16 -0500

    Added the priority queue class

---
+++ src/PathfinderVisualizer/PriorityQueue.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PathfinderVisualizer
+{
+    class PriorityQueue<T>
+    {
+        private Dictionary<T, int> data;
+
+        public PriorityQueue()
+        {
+            data = new Dictionary<T, int>();
+        }
+        public void Enqueue(T item, int priority)
+        {
+            if (data.ContainsKey(item))
+                data.Remove(item);
+            data[item] = priority;
+        }
+        public T Dequeue()
+        {
+            if (data.Count == 0)
+                throw new Exception("Queue is empty");
+
+            T bestItem = default(T);
+            int bestPriority = int.MaxValue;
+
+            foreach (var item in data)
+                if (item.Value <= bestPriority)
+                {
+                    bestItem = item.Key;
+                    bestPriority = item.Value;
+                }
+
+            data.Remove(bestItem);
+            return bestItem;
+        }
+        public void Clear()
+        {
+            data.Clear();
+        }
+        public int Count()
+        {
+            return data.Count;
+        }
+        public bool Contains(T item)
+        {
+            return data.ContainsKey(item);
+        }
+        public override string ToString()
+        {
+            string s = "{";
+            foreach (var item in data.Keys)
+                s += item.ToString() + ", ";
+            s += "}, count = " + data.Count;
+            return s;
+        }
+    }
+}
