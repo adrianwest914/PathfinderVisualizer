7bd4cdd3d73db84d591d93fb852275c520e6b9b6
Opuli1 <72239450+Opuli1@users.noreply.github.com>
2021-04-09 19:57:33 -0500

    Added brushes that are used for the different type of tiles

--- src/PathfinderVisualizer/App.xaml.cs
+++ src/PathfinderVisualizer/App.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Media;
+
 
 namespace PathfinderVisualizer
 {
@@ -13,5 +9,14 @@ namespace PathfinderVisualizer
     /// </summary>
     public partial class App : Application
     {
+        public static readonly Brush SDefault = System.Windows.Media.Brushes.Gray;
+        public static readonly Brush SWall = System.Windows.Media.Brushes.Black;
+        public static readonly Brush SWeight = System.Windows.Media.Brushes.DarkBlue;
+        public static readonly Brush SGoal = System.Windows.Media.Brushes.Red;
+        public static readonly Brush SStart = System.Windows.Media.Brushes.Green;
+        public static readonly Brush SDefaultVisited = new SolidColorBrush(Color.FromRgb(50, 50, 50));
+        public static readonly Brush SWeightVisited = System.Windows.Media.Brushes.LightSkyBlue;
+        public static readonly Brush SDefaultPath = System.Windows.Media.Brushes.White;
+        public static readonly Brush SWeightPath = System.Windows.Media.Brushes.White;
     }
 }
